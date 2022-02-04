b003feb7abc80782e80b82a25cdcb36e00441aa9
bakyt92 <bakyt92@mail.ru>
2022-02-04 03:04:16 +0300

    4 Febr 2022 - Home

--- push_swap.c
+++ push_swap.c
@@ -1,15 +1,35 @@
 #include "push_swap.h"
 
-int main(int argc, int **argv)
+void	sort(s_allData *stacks)
 {
-	s_Node *first;
-	s_process *start;
 
+}
 
+void	free_stack(s_allData *stacks)
+{
 
+}
 
-	printf("Hello, World!\n");
+s_allData	*read_args(int argc, char **argv)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	s_allData *stacks;
+
+	stacks->stack_a = NULL;
+	stacks->stack_b = NULL;
+
+	if (argc < 2)
+		return (0);
+	else
+	{
+		stacks = read_args(argc, argv);
+		sort(stacks);
+		free_stack(stacks);
+	}
 	return 0;
 }
 
-t_node ft_sort ()
--- push_swap.h
+++ push_swap.h
@@ -2,6 +2,7 @@
 #define INC_05_1_PUSH_SWAP_PUSH_SWAP_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct t_node {
 	int value;
@@ -11,9 +12,14 @@ typedef struct t_node {
 	struct t_node *prev;
 } s_Node;
 
-typedef struct t_process {
-	int x;
-	int y;
-} s_process;
+typedef struct t_allData {
+	s_Node *stack_a;
+	s_Node *stack_b;
+	int median;
+	int size_a;
+	int size_b;
+	int min;
+	int max;
+} s_allData;
 
 #endif
